d571553fb5d20de6e7bccc24d0a39027770faf47
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-04-28 17:31:08 +0800

    horse race tests clone

--- python/pygame/hrt.py
+++ python/pygame/hrt.py
@@ -5,12 +5,16 @@ import random
     # initializing game #
 screen = pygame.display.set_mode((800, 600)) # set screen size (width, height)
 clock = pygame.time.Clock() # set fps
-velocity = 2 # speed of horse
+velocity = 1.1 # speed of horse
 running = True # keep game loop running
+    #-- mask
 maze_image = pygame.image.load("assets/map_test.png").convert()  # convert for faster pixel access
 maze_mask = pygame.mask.from_threshold(maze_image, (0, 0, 0), (10, 10, 10)) #convert maze png to mask
+    #-- mask
+    #-- velocity
 v_temp1 = random.choice([-1, 1]) * velocity
 v_temp2 = random.choice([-1, 1]) * velocity
+    #-- velocity
     # initializing game #
 
     # initialize horse
@@ -27,29 +31,22 @@ class Horse:
         surface.blit(self.image, self.rect)
     # initialize horse
 
-def when_bounced(x, y):
-    x *= 5
-    y *= 5
-    return 
+def when_bounced(horse):
+    horse.velocity_x *= 1.0001
+    horse.velocity_y *= 1.0001
+    return horse
 
 def horse_bounce(horse):
-    if(horse.rect.left < 0): #hits left
-        horse.velocity_x = 1
-        when_bounced(horse.velocity_x, horse.velocity_y)
-    elif(horse.rect.right > 800): # hits right
-        horse.velocity_x = -1
-        when_bounced(horse.velocity_x, horse.velocity_y)
-    elif(horse.rect.top < 0): # hits top
-        horse.velocity_y = 1
-        when_bounced(horse.velocity_x, horse.velocity_y)
-    elif(horse.rect.bottom > 600): # hits bottom
-        horse.velocity_y = -1
-        when_bounced(horse.velocity_x, horse.velocity_y)
+    if(horse.rect.left < 0 or horse.rect.right > 800): #hits left or right
+        horse.velocity_x *= -1
+    elif(horse.rect.top < 0 or horse.rect.bottom > 600): # hits top or bottom
+        horse.velocity_y *= -1
+    when_bounced(horse)
 
 def move_horse(horse):
+    horse_bounce(horse)
     horse.x += horse.velocity_x
     horse.y += horse.velocity_y
-    horse_bounce(horse)
     horse.rect.center = (horse.x, horse.y)
 
 horse1 = Horse(100, 100, "assets/horse_1.png", v_temp1, v_temp2) # <-- place horse
