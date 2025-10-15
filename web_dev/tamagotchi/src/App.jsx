aac5fdb8b5e73a8db03143d849ba48e9c7e70f04
amabella-aguiluz <amabellaaguiluz1@gmail.com>
2025-10-15 17:06:33 +0800

    fixed spacing + white background

--- web_dev/tamagotchi/src/App.jsx
+++ web_dev/tamagotchi/src/App.jsx
@@ -12,10 +12,12 @@ import sprite2 from './assets/sprites/1.png'
 import sprite3 from './assets/sprites/2.png'
 import sprite4 from './assets/sprites/3.png'
 
+"#f4fbf8ff"
+
 function StatButton({ icon, statName, onClick}) {
   return (<button
   onClick={() => onClick(statName, 10)}
-  className="w-[110px] h-[110px] p-[13px] flex items-center justify-center bg-transparent rounded-[25px] box-border">
+  className="w-[110px] h-[110px] p-[13px] flex items-center justify-center bg-transparent rounded-[25px] border-5 border-[#7c6dddff]">
     <img src={icon} className="w-[50px] h-[50px]"></img></button>)
 }
 
@@ -29,10 +31,10 @@ function StatBar({ icon, value }) {
   else color = "#F38462";
 
   return (
-    <div id="mood_bar" className="w-[412px] h-[55px] flex flex-row items-center gap-[10px] justify-center px-[30px] py-[5px]">
+    <div id="mood_bar" className="w-[412px] h-[35px] flex flex-row items-center gap-[10px] justify-center px-[30px] py-[5px]">
       <img src={icon} alt="icon" className="w-[35px] h-[35px]" />
       <div
-        id="bar_frame" className="relative w-[307px] h-[35px] rounded-[30px] bg-white border-gray-200 box-border"
+        id="bar_frame" className="relative w-[307px] h-[35px] rounded-[30px] bg-[#f4fbf8ff] border-[#f4fbf8ff] border-5"
       >
         <div
           className="h-full rounded-full transition-all duration-50"
@@ -63,7 +65,12 @@ function App() {
       ...prev,
       [name]:Math.min(100, Math.max(0, prev[name] + change)),
     }));
+
+    setCurrentAction(name);
+    setTimeout(() => setCurrentAction(null), 1000); // 2 seconds
   };
+
+  const [currentAction, setCurrentAction] = useState(null);
 
   // decay stat
 useEffect(() => {
@@ -85,42 +92,47 @@ useEffect(() => {
 
     // update sprite
   let sprite;
+
+  if (currentAction === 'love') {
+  sprite = sprite1;
+} else if (currentAction === 'hunger') {
+  sprite = sprite1;
+} else if (currentAction === 'energy') {
+  sprite = sprite1;
+} else {
   if(average >= 90) sprite = sprite1;
   else if(average >= 60) sprite = sprite2;
   else if(average >= 40) sprite = sprite3;
-  else sprite = sprite4;
+  else sprite = sprite4;}
 
 
   // visuals 
   return (
       <div className="w-screen h-screen relative flex items-center justify-center">
-        <div id="home" className="w-[412px] h-[917px] flex flex-col items-center justify-start bg-gradient-to-b from-[#CBA9FF] to-[#8CB4FF]">
+        <div id="home" className="w-full max-w-[412px] flex flex-col items-center justify-start bg-gradient-to-b from-[#CBA9FF] to-[#8CB4FF] overflow-hidden">
           <div id="main" className="w-full flex flex-col items-center justify-start">
-          {/* stats 
-          TODO: WHITE BACKGROUND*/}
-        <div id = "bar-holder" className="w-[412px] h-[180px] flex flex-col items-center justify-center mt-[20px] bg-white">
+          {/* stats */}
+        <div id = "bar-holder" className="w-[412px] h-[160px] flex flex-col items-center justify-center py-[10px] gap-[10px]">
         <StatBar icon={heartIcon} value={stats.love} />
         <StatBar icon={candyIcon} value={stats.hunger} />
         <StatBar icon={bedIcon} value={stats.energy} />
       </div>
-        {/* sprite 
-        TODO: FIX SPACING*/}
+        {/* sprite */}
         <div id="character-frame" className="w-[412px] h-[597px]">
           <div id="holding-frame" className="w-[412px] h-[597px] relative">
             <img 
     src={base} 
-    className="h-[588px] absolute bottom-0 left-1/2 transform -translate-x-1/2"></img>
+    className="h-[597px] absolute bottom-0 left-1/2 transform -translate-x-1/2"></img>
             <img 
   src={sprite} 
-  className="h-[588px] absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10" />
+  className="h-[597px] absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10" />
           </div>
         </div>
 
 
-        {/*buttons
-        TODO: WHITE BACKGROUND*/}
-        <div id="button-frame" className="w-[412px] h-[140px] px-[25px] py-[15px] flex flex-col items-center justify-center bg-white">
-          <div id="button-rows" className="w-[362px] h-[110px] justify-center items-center flex flex-row gap-[10px]">
+        {/*buttons */}
+        <div id="button-frame" className="w-[412px] h-[140px] flex flex-col items-center justify-center bg-[#f4fbf8ff]">
+          <div id="button-rows" className="w-[362px] h-full justify-center items-center flex flex-row gap-[10px]">
             <StatButton icon={heartIcon} statName="love" onClick={updateStat} />
             <StatButton icon={candyIcon} statName="hunger" onClick={updateStat} />
             <StatButton icon={bedIcon} statName="energy" onClick={updateStat} />
